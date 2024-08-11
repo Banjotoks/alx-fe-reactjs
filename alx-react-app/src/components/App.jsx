4c1f88e21745ba518d9e0939312bfb6f262a7297
Banjo Adetokunbo <banjotokunboh@gmail.com>
2024-08-11 12:27:31 +0100

    Building my first react app

--- alx-react-app/src/components/App.jsx
+++ alx-react-app/src/components/App.jsx
@@ -1,8 +1,7 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import WelcomeMessage from './components/WelcomeMessage';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,8 +16,6 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <WelcomeMessage />
-
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
