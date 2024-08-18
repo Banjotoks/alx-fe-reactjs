f9217d1b4ec4c85c503cac23cac050260e7bc7ca
Banjo Adetokunbo <banjotokunboh@gmail.com>
2024-08-18 21:05:17 +0100

    My Company app

---
+++ my-company/src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Browswer Router as Router, Routes and Route } from 'react router dom';
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+function App() {
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/" element={<About />} />
+                <Route path="/" element={<Services />} />
+                <Route path="/" element={<Contact />} />
+
+
+            </Routes>
+
+            <Footer />
+
+
+        </Router>
+
+    );
+}
+
+export default App;
---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+      <footer style={{ padding: '10px', backgroundColor: 'Blue', color: 'Black', textAlign: 'center' }}>
+        <p>&copy; 2024 My Company. All Rights Reserved.</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
