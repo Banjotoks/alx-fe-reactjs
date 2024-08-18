5018ab6327859f0227bb5c24147b2eae7d9dcb75
Banjo Adetokunbo <banjotokunboh@gmail.com>
2024-08-18 22:13:38 +0100

    My Company

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -9,23 +9,17 @@ import Footer from "./components/Footer";
 
 function App() {
     return (
-        <Router>
-          <div>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/" element={<About />} />
-                <Route path="/" element={<Services />} />
-                <Route path="/" element={<Contact />} />
-
-
-            </Routes>
-            </div>
-
-            <Footer />
-
-
-        </Router>
+      <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
 
     );
 }
