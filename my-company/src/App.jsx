df0068242d9367648dd7fe4f3520d6751dfbebd9
Banjo Adetokunbo <banjotokunboh@gmail.com>
2024-08-18 21:34:36 +0100

    My company app

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Browswer Router as Router, Routes and Route } from 'react router dom';
+import { BrowswerRouter as Router, Routes, Route } from 'react router dom';
 import Home from "./components/Home";
 import About from "./components/About";
 import Services from "./components/Services";
