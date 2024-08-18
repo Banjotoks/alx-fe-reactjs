7a115098ae834fed78d0221e23f2f9bb79be5eec
Banjo Adetokunbo <banjotokunboh@gmail.com>
2024-08-18 21:53:52 +0100

    My company app

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,6 +3,14 @@ import { link } from react-router-dom;
 
 function Navbar() {
     return (
+        <nav style={{ padding: '10px', backgroundColor: 'blue', color: 'black' }}>
+        <link to="/" style={{ padding: '15px', color: 'black' }}>Home</link>
+        <link to="/about" style={{ padding: '15px', color: 'black' }}>About</link>
+        <link to="/services" style={{ padding: '15px', color: 'black' }}>Services</link>
+        <Link to="/contact" style={{padding: '15px', color:'black'}}>Contact</Link>
+        </nav>
+
+        
 
     );
 }
