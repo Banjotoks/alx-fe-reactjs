19c6b985d033c1b0dbb013cd3c07616355face5d
Banjo Adetokunbo <banjotokunboh@gmail.com>
2024-08-26 21:14:42 +0100

    recipe

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,10 +1,13 @@
+import { Navigate, useNavigate } from "react-router-dom";
 import { useRecipeStore } from recipeStore;
 
 const DeleteRecipeButton = ({ recipeId }) => {
     const deleteRecipe = useRecipeStore(state => state.deleteRecipe);
+    const navigate = useNavigate();
 
     const handleDelete = () => {
         deleteRecipe(recipeId);
+        Navigate('/')
     };
 
     return(
