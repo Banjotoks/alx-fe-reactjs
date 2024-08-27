de18b2d685b8007817490a191abfdad2ae1ad5f9
Banjo Adetokunbo <banjotokunboh@gmail.com>
2024-08-27 09:59:35 +0100

    Recipe

--- recipe-sharing-app/src/components/AddRecipeForm.jsx
+++ recipe-sharing-app/src/components/AddRecipeForm.jsx
@@ -19,6 +19,7 @@ const AddRecipeForm = () => {
       addRecipe({ id: Date.now(), title, description });
       setTitle('');
       setDescription('');
+      }
     };
 
     return (
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,6 +2,22 @@ import { create } from 'zustand'
 
 const useRecipeStore = create(set => ({
     recipes: [],
+    searchTerm: '',
+    filteredRecipes: [],
+
+    setSearchTerm: (term) => set(state => {
+      const filteredRecipes = state.recipes.filter(recipe =>
+        recipe.title.toLowerCase().includes(term.toLowerCase())
+      );
+      return { searchTerm: term, filteredRecipes };
+    }),
+
+    filterRecipes: () => set(state => ({
+      filteredRecipes: state.recipes.filter(recipe =>
+        recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+      )
+    })),
+
     addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
     deleteRecipe: (id) =>set(state => ({recipes: state.recipes.filter(recipe => recipe.id !==id), })),
     updateRecipe: (updatedRecipe) => set(state => ({recipes: state.recipes.map(recipe => recipe.id === updatedRecipe.id ? updatedRecipe : recipe), })), 
