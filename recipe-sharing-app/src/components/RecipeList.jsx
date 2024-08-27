9441d69b494d5f73161069ce1b40f52098e3395f
Banjo Adetokunbo <banjotokunboh@gmail.com>
2024-08-27 10:39:18 +0100

    Recipe

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,26 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import useRecipeStore from "./recipeStore";
 
 const RecipeList = () => {
     const recipes = useRecipeStore(state => state.recipes);
-    const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+    const filterRecipes = useRecipeStore((state) => state.deleteRecipe);
+    const searchTerm = useRecipeStore((state) => state.searchTerm);
+
+    useEffect(() => {
+        ilterRecipes();
+    }, [searchTerm, filterRecipes]);
 
     return (
         <div>
-            {recipes.map(recipe => (
-                <div key={recipe.id}>
-                    <h3>{recipe.title}</h3>
-                    <p>{recipe.description}</p>
-                    <button onClick={() => deleteRecipe(recipe.id)}>Delete</button>
-
-
-                </div>
-            ))}
-        
+          {recipes.length > 0 ? (
+            recipes.map(recipe => (
+              <div key={recipe.id}>
+                <h3>{recipe.title}</h3>
+                <p>{recipe.description}</p>
+                <button onClick={() => deleteRecipe(recipe.id)}>Delete</button>
+              </div>
+            ))
+          ) : (
+            <p>No recipes found</p>
+          )}
         </div>
-
-    );
-};
+      );
+    };
 
 
 export default RecipeList;
