da78d426c2165d10781e1b463c10f1eefcd5ca58
Banjo Adetokunbo <banjotokunboh@gmail.com>
2024-09-01 09:12:53 +0100

    form

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import RegistrationForm from './components/RegistrationForm'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <div>
+      <h1>User Registration</h1>
+      <RegistrationForm />
+    </div> 
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
