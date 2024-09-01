2bd2e77be1ed09e7292606ed85ddf1b53f68ddd2
Banjo Adetokunbo <banjotokunboh@gmail.com>
2024-09-01 22:56:35 +0100

    Advanced Router

---
+++ react-router-advanced/src/components/ProfileSettings.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ProfileSettings() {
+  return (
+    <div>
+      <h2>Profile Settings</h2>
+      <p>My profile settings here.</p>
+    </div>
+  );
+}
+
+export default ProfileSettings;
