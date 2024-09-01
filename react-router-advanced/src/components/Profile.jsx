3f42c7fe55801346cc6affdb1066ef6d83fd9e67
Banjo Adetokunbo <banjotokunboh@gmail.com>
2024-09-01 23:27:44 +0100

    router

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function Profile() {
   return (
