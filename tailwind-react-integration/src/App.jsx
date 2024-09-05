95524a6d499e479a114a41bb72f6bf53597ffaf1
Banjo Adetokunbo <banjotokunboh@gmail.com>
2024-09-05 19:06:06 +0100

    React JS with a CSS Framework

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserProfile from './components/UserProfile'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
+    <div className='App'>
+      <UserProfile />
+
+    </div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -17,14 +22,11 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-      </div>
+      
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
