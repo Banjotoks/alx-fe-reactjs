37ba053d09429cc5669f1b2d4a676513e1437e30
Banjo Adetokunbo <banjotokunboh@gmail.com>
2024-09-06 16:45:18 +0100

    Tailwind.css

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function UserProfile() {
     return (
-      <div className="user-profile bg-gray-100 md:p-8 sm:p-4 md:max-w-sm sm:max-w-sx mx-auto my-20 rounded-lg shadow-lg">
-        <img src="https://via.placeholder.com/150" alt="User" className="rounded-full md:w-36 h-36, sm:w-24 h-24 mx-auto" />
+      <div className="user-profile bg-gray-100 md:p-8 sm:p-4 md:max-w-sm sm:max-w-xs mx-auto my-20 rounded-lg shadow-lg">
+        <img src="https://via.placeholder.com/150" alt="User" className="rounded-full md:w-36 h-36 sm:w-24 h-24 mx-auto" />
         <h1 className="sm:text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
         <p className="text-gray-600 sm:text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
       </div>
