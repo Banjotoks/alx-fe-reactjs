e1882b28709c2e306c34cdf078324e970f93abe7
Banjo Adetokunbo <banjotokunboh@gmail.com>
2024-09-06 13:17:18 +0100

    React JS with a CSS Framework

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 function UserProfile() {
     return (
-      <div className="user-profile bg-gray-100 p-4 sm:p-8 max-w-sx sm:max-w-sm mx-auto, my-20 rounded-lg shadow-lg">
-        <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-36 h-36 sm:w-24 h-24 mx-auto" />
-        <h1 className="text-lg sm:text-xl text-blue-800 my-4">John Doe</h1>
-        <p className="text-gray-600 text-sm" sm:text-base>Developer at Example Co. Loves to write code and explore new technologies.</p>
+      <div className="user-profile bg-gray-100 md:p-4 sm:p-4 sm:max-w-sx md:max-w-sm mx-auto, my-20 rounded-lg shadow-lg">
+        <img src="https://via.placeholder.com/150" alt="User" className="rounded-full md:w-36 h-36 sm:w-24 h-24 mx-auto" />
+        <h1 className="sm:text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
+        <p className="text-gray-600 sm:text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
       </div>
     );
   }
