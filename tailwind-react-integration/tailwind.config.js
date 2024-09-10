1a7b628b38ba4f3f68ffaf68b0b28efb2e923d67
Banjo Adetokunbo <banjotokunboh@gmail.com>
2024-09-10 16:45:04 +0100

    Tailwind integration

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: ["./public/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false,
   theme: {
     extend: {},
