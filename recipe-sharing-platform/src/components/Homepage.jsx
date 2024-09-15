6d1e9e4c408caed5d60d6bd03d115d5b2121d501
Banjo Adetokunbo <banjotokunboh@gmail.com>
2024-09-15 18:07:29 +0100

    recipe

--- recipe-sharing-platform/src/components/Homepage.jsx
+++ recipe-sharing-platform/src/components/Homepage.jsx
@@ -5,7 +5,7 @@ const HomePage = () => {
     const[recipes, setRecipes] = useState([]);
 
     useEffect(() => {
-        setRecipes(recipesData); 
+        setRecipes(recipesData);  
     }, []);
 
 
