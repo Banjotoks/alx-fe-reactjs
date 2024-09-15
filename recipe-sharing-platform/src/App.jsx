db4b27825a3d9a8e8e309755b6702b1958f3e84e
Banjo Adetokunbo <banjotokunboh@gmail.com>
2024-09-15 21:35:45 +0100

    recipe

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './components/Homepage';
+import HomePage from '../components/HomePage';
 import RecipeDetail from './components/RecipeDetail';
 
 function App() {
